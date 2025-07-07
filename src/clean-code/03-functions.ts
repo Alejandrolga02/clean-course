561bfaea6cba439ab15d4408caf83515f56d91bc
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2025-07-06 17:39:36 -0700

    add funciones codigo limpio

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,34 @@
+(() => {
+	function getMovieById(movieId: string) {
+		console.log({ movieId });
+	}
+
+	function getMovieCastById(movieId: string) {
+		console.log({ movieId });
+	}
+
+	function getActorBioById(actorId: string) {
+		console.log({ actorId });
+	}
+
+	interface Movie {
+		title: string, 
+		description: string, 
+		rating: number, 
+		cast: string[]
+	}
+
+	function createMovie({ title, description, rating, cast }: Movie) {
+		console.log({ title, description, rating, cast });
+	}
+
+	function createActor(fullName: string, birthdate: Date): boolean {
+		// tarea asincrona para verificar nombre
+		// ..
+		// ..
+		if (fullName === 'fernando') return false;
+
+		console.log('Crear actor');
+		return true;
+	}
+})();
