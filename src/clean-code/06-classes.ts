074c69976f73a14a729fd062d9554b466518d4f5
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2025-07-07 20:24:34 -0700

    agregar ejercicio herencia mal aplicado

--- src/clean-code/06-classes.ts
+++ src/clean-code/06-classes.ts
@@ -14,9 +14,54 @@
 	}
 
 	class User extends Person {
-		
+		public lastAccess: Date;
+
+		constructor(
+			public email: string,
+			public role: string,
+			name: string,
+			gender: Gender,
+			birthdate: Date
+		){
+			super(name, gender, birthdate);
+			this.lastAccess = new Date();
+		}
+
+		checkCredentials(): boolean {
+			return true;
+		}
+	}
+
+	class UserSettings extends User {
+		constructor(
+			public workingDirectory: string,
+			public lastOpenFolder: string,
+			email: string,
+			role: string,
+			name: string,
+			gender: Gender,
+			birthdate: Date,
+		) {
+			super(email, role, name, gender, birthdate);
+		}
 	}
 
 	const newPerson = new Person('Alejandro Ivan Lizarraga Rojas', 'M', new Date('2002-10-08'));
+
+	const newUserSettings = new UserSettings(
+		'/usr/home',
+		'/home',
+		'alejandroivanliza@gmail.com',
+		'Admin',
+		'Alejandro Ivan Lizarraga Rojas',
+		'M',
+		new Date('2002-10-08')
+	)
+
+	console.log({ 
+		newUserSettings, 
+		isCredentialsValid: newUserSettings.checkCredentials() 
+	});
+
 	console.log({ newPerson });
 })()
