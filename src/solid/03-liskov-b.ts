0806c3ea1c4b2cb60681d8dbfea20f92c1751456
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2025-08-13 19:56:22 -0700

    refactor: simplify car seat printing by using ICar interface

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,35 +1,48 @@
-export class Tesla {
-
-    constructor( private numberOfSeats: number ) {}
-
-    getNumberOfTeslaSeats() {
-        return this.numberOfSeats;
-    }
+export interface ICar {
+	getNumberOfSeats: () => number
+	getBrand: () => string
 }
 
-export class Audi {
 
-    constructor( private numberOfSeats: number ) {}
+export class Car implements ICar {
 
-    getNumberOfAudiSeats() {
+    constructor(private numberOfSeats: number, private readonly brand: string) {}
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
+
+	getBrand() {
+		return this.brand;
+	}
 }
 
-export class Toyota {
-
-    constructor( private numberOfSeats: number ) {}
-
-    getNumberOfToyotaSeats() {
-        return this.numberOfSeats;
-    }
+export class Tesla extends Car {
+    constructor(numberOfSeats: number ) {
+		super(numberOfSeats, 'Tesla')
+	}
 }
 
-export class Honda {
+export class Audi extends Car {
+    constructor(numberOfSeats: number ) {
+		super(numberOfSeats, 'Audi')
+	}
+}
 
-    constructor( private numberOfSeats: number ) {}
+export class Toyota extends Car {
+    constructor(numberOfSeats: number ) {
+		super(numberOfSeats, 'Toyota')
+	}
+}
 
-    getNumberOfHondaSeats() {
-        return this.numberOfSeats;
-    }
+export class Honda extends Car {
+    constructor(numberOfSeats: number ) {
+		super(numberOfSeats, 'Honda')
+	}
+}
+
+export class Volvo extends Car {
+    constructor(numberOfSeats: number ) {
+		super(numberOfSeats, 'Volvo')
+	}
 }
